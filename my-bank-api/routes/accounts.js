4286007afde23a46be19b9e8fbf7408b616ae44a
WellingtonFidelis <wellingtonf20@gmail.com>
2020-08-08 09:47:45 -0300

    DELETE, PUT and PATCH

--- my-bank-api/routes/accounts.js
+++ my-bank-api/routes/accounts.js
@@ -59,4 +59,69 @@ router.get('/:id', async (request, response) => {
   }
 });
 
+router.delete('/:id', async (request, response) => {
+  try {
+    const data = JSON.parse(await fs.readFile(global.fileNameJson));
+    // console.log(JSON.stringify(data));
+    // console.log(data.accounts);
+    data.accounts = data.accounts.filter(
+      (account) => account.id !== parseInt(request.params.id)
+    );
+    // console.log(data.accounts);
+
+    await fs.writeFile(global.fileNameJson, JSON.stringify(data, null, 2));
+    // request.params.id
+    response.send('<h1>Account deleted!</h1>');
+    // response.end();
+  } catch (error) {
+    response.status(400).send({
+      error: error.message,
+    });
+  }
+});
+
+// atualizar o recurso de forma integral
+router.put('/', async (request, response) => {
+  try {
+    const account = request.body;
+    const data = JSON.parse(await fs.readFile(global.fileNameJson));
+
+    const index = data.accounts.findIndex(
+      (accountIndex) => accountIndex.id === account.id
+    );
+
+    data.accounts[index] = account;
+
+    await fs.writeFile(global.fileNameJson, JSON.stringify(data, null, 2));
+
+    response.send(account);
+  } catch (error) {
+    response.status(400).send({
+      error: error.message,
+    });
+  }
+});
+
+// atualização parcial
+router.patch('/updateBalance', async (request, response) => {
+  try {
+    const account = request.body;
+    const data = JSON.parse(await fs.readFile(global.fileNameJson));
+
+    const index = data.accounts.findIndex(
+      (accountIndex) => accountIndex.id === account.id
+    );
+
+    data.accounts[index].balance = account.balance;
+
+    await fs.writeFile(global.fileNameJson, JSON.stringify(data, null, 2));
+
+    response.send(data.accounts[index]);
+  } catch (error) {
+    response.status(400).send({
+      error: error.message,
+    });
+  }
+});
+
 export default router;
