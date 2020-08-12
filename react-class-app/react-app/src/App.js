afbd2b85082984fbba422e99ecf80f2d8717eb19
WellingtonFidelis <wellingtonf20@gmail.com>
2020-08-12 20:30:30 -0300

    Modifications on App.js and put function timestamp

--- react-class-app/react-app/src/App.js
+++ react-class-app/react-app/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+import { getNewTimeStamp } from './helpers/timestamp';
+
+export default function App() {
+  const [clickArray, setClickArray] = useState([]);
+
+  useEffect(() => {
+    document.title = clickArray.length;
+  });
+
+  const handleClick = () => {
+    const newClickArray = Object.assign([], clickArray);
+    newClickArray.push(getNewTimeStamp());
+
+    setClickArray(newClickArray);
+
+    // this.setState({ clickArray, newClickArray });
+  };
+
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>
+        React e <em>Hookd</em>
+      </h1>
+      <button onClick={handleClick}>Click here!</button>
+
+      <ul>
+        {clickArray.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
     </div>
   );
 }
-
-export default App;
