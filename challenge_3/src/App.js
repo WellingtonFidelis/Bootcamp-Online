a8d34ef21ad164339ac96a12690b39b5f7211cf8
WellingtonFidelis <wellingtonf20@gmail.com>
2020-08-13 19:56:50 -0300

    part 1 finished

--- challenge_3/src/App.js
+++ challenge_3/src/App.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allCountries: [],
+    };
+  }
+
+  // good place to make requests http
+  async componentDidMount() {
+    const response = await fetch('https://restcountries.eu/rest/v2/all');
+    const json = await response.json();
+
+    // console.log(json);
+
+    const allCountries = json.map(({ name, numericCode, flag, population }) => {
+      return {
+        id: numericCode,
+        name,
+        flag,
+        population,
+      };
+    });
+
+    this.setState({
+      // allCountries: allCountries,
+      allCountries,
+    });
+
+    // console.log(allCountries);
+  }
+
   render() {
-    return <ProjetoBase />;
+    const { allCountries } = this.state;
+
+    return (
+      <div className="container">
+        <h1>React countries</h1>
+      </div>
+    );
   }
 }
